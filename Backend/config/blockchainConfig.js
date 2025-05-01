d1e29f2b0d04c980dcb08648605c938a0144d547
Dharaneedharan G K <139697363+imdharanee@users.noreply.github.com>
2025-05-01 17:17:35 +0530

    Update blockchainConfig.js

--- Backend/config/blockchainConfig.js
+++ Backend/config/blockchainConfig.js
@@ -2,7 +2,7 @@ import Web3 from "web3";
 import contractABI from "../../Blockchain/build/contracts/PatientRecords.json" assert { type: "json" };
 
 const web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
-const contractAddress = "0x51441E1093EE71B1451732845AECB477B912cF01";
+const contractAddress = "0x51441E1093EE71B1451732845AECB477B912cF022222";
 const contract = new web3.eth.Contract(contractABI.abi, contractAddress);
 
 export { web3, contract };
